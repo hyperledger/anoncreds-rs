b0069fea30ebdccf4a0f4c810e09ac52c9a50d98
Andrew Whitehead <cywolf@gmail.com>
2020-07-07 12:00:56 -0700

    remove unused macros, make handle type validatable
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- indy-utils/src/macros.rs
+++ indy-utils/src/macros.rs
@@ -9,63 +9,6 @@ macro_rules! unwrap_opt_or_return {
 }
 
 #[macro_export]
-macro_rules! unwrap_or_return {
-    ($result:expr, $err:expr) => {
-        match $result {
-            Ok(res) => res,
-            Err(_) => return $err,
-        };
-    };
-}
-
-#[macro_export]
-macro_rules! unwrap_or_map_return {
-    ($result:expr, $on_err:expr) => {
-        match $result {
-            Ok(res) => res,
-            Err(err) => return ($on_err)(err),
-        };
-    };
-}
-
-#[macro_export]
-macro_rules! in_closure {
-    ($($e:tt)*) => {(|| -> Result<_, _> {$($e)*})()}
-}
-
-#[macro_export]
-macro_rules! assert_match {
-    ($pattern:pat, $var:expr) => {
-        assert!(match $var {
-            $pattern => true,
-            _ => false,
-        })
-    };
-    ($pattern:pat, $var:expr, $val_in_pattern:ident, $exp_value:expr) => {
-        assert!(match $var {
-            $pattern => $val_in_pattern == $exp_value,
-            _ => false,
-        })
-    };
-    ($pattern:pat, $var:expr, $val_in_pattern1:ident, $exp_value1:expr, $val_in_pattern2:ident, $exp_value2:expr) => {
-        assert!(match $var {
-            $pattern => $val_in_pattern1 == $exp_value1 && $val_in_pattern2 == $exp_value2,
-            _ => false,
-        })
-    };
-}
-
-#[macro_export]
-macro_rules! assert_kind {
-    ($kind:expr, $var:expr) => {
-        match $var {
-            Err(e) => assert_eq!($kind, e.kind()),
-            _ => assert!(false, "Result expected to be error"),
-        }
-    };
-}
-
-#[macro_export]
 macro_rules! new_handle_type (($newtype:ident, $counter:ident) => (
 
     lazy_static! {
@@ -80,6 +23,7 @@ macro_rules! new_handle_type (($newtype:ident, $counter:ident) => (
         pub fn invalid() -> $newtype {
             $newtype(0)
         }
+
         #[allow(dead_code)]
         pub fn next() -> $newtype {
             $newtype($counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1)
@@ -97,6 +41,16 @@ macro_rules! new_handle_type (($newtype:ident, $counter:ident) => (
         type Target = usize;
         fn deref(&self) -> &usize {
             &self.0
+        }
+    }
+
+    impl $crate::Validatable for $newtype {
+        fn validate(&self) -> Result<(), $crate::ValidationError> {
+            if(**self == 0) {
+                Err("Invalid handle: zero".into())
+            } else {
+                Ok(())
+            }
         }
     }
 ));
