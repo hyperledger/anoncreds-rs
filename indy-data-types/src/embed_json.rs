5fa374fadb94892b1c06aa4a6469f6e0564f2030
Andrew Whitehead <cywolf@gmail.com>
2020-07-08 12:01:01 -0700

    add helpers to EmbedJson
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- indy-data-types/src/embed_json.rs
+++ indy-data-types/src/embed_json.rs
@@ -3,16 +3,6 @@ use std::fmt;
 use std::marker::PhantomData;
 use std::ops;
 
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-#[cfg(feature = "serde")]
-use serde_json::value::RawValue;
-
-#[cfg(feature = "serde")]
-pub trait EmbedExtract {
-    type Inner: Serialize + for<'de> Deserialize<'de>;
-}
-
 pub struct EmbedJson<T> {
     _pd: PhantomData<T>,
     pub inner: String,
@@ -24,6 +14,12 @@ impl<T> EmbedJson<T> {
             _pd: PhantomData,
             inner,
         }
+    }
+}
+
+impl<T> From<String> for EmbedJson<T> {
+    fn from(inner: String) -> Self {
+        Self::from_string(inner)
     }
 }
 
@@ -50,7 +46,7 @@ impl<T> fmt::Debug for EmbedJson<T> {
 
 impl<T> fmt::Display for EmbedJson<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", &self.inner)
+        f.write_str(self.inner.as_str())
     }
 }
 
@@ -75,17 +71,6 @@ impl<T> Ord for EmbedJson<T> {
 }
 
 #[cfg(feature = "serde")]
-impl<T: EmbedExtract> EmbedJson<T> {
-    pub fn embed(value: &T::Inner) -> Result<Self, crate::ConversionError> {
-        Ok(serde_json::to_string(value).map(Self::from_string)?)
-    }
-
-    pub fn extract(&self) -> Result<T::Inner, crate::ConversionError> {
-        Ok(serde_json::from_str(&self.inner)?)
-    }
-}
-
-#[cfg(feature = "serde")]
 pub fn embed_json<T: EmbedExtract>(
     value: &T::Inner,
 ) -> Result<EmbedJson<T>, crate::ConversionError> {
@@ -93,24 +78,66 @@ pub fn embed_json<T: EmbedExtract>(
 }
 
 #[cfg(feature = "serde")]
-impl<T> Serialize for EmbedJson<T> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let raw = unsafe { std::mem::transmute::<&str, &RawValue>(&self.inner) };
-        raw.serialize(serializer)
-    }
+pub trait EmbedExtract {
+    type Inner: serde::Serialize + for<'de> serde::Deserialize<'de>;
 }
 
 #[cfg(feature = "serde")]
-impl<'de, T> Deserialize<'de> for EmbedJson<T> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let rawval = <&RawValue>::deserialize(deserializer)?;
-        Ok(EmbedJson::from_string(rawval.get().to_string()))
+mod serde_support {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use serde_json::value::{RawValue, Value};
+    use std::convert::TryFrom;
+
+    use super::{EmbedExtract, EmbedJson};
+    use crate::ConversionError;
+
+    impl<T: EmbedExtract> EmbedJson<T> {
+        pub fn embed(value: &T::Inner) -> Result<Self, crate::ConversionError> {
+            Ok(serde_json::to_string(value).map(Self::from_string)?)
+        }
+
+        pub fn extract(&self) -> Result<T::Inner, crate::ConversionError> {
+            Ok(serde_json::from_str(&self.inner)?)
+        }
+    }
+
+    impl<T> Serialize for EmbedJson<T> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let raw = unsafe { std::mem::transmute::<&str, &RawValue>(&self.inner) };
+            raw.serialize(serializer)
+        }
+    }
+
+    impl<'de, T> Deserialize<'de> for EmbedJson<T> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let rawval = <&RawValue>::deserialize(deserializer)?;
+            Ok(EmbedJson::from_string(rawval.get().to_string()))
+        }
+    }
+
+    impl<T> EmbedJson<T> {
+        pub fn from_value(value: Value) -> Result<Self, ConversionError> {
+            let inner = serde_json::to_string(&value)?;
+            Ok(Self::from_string(inner))
+        }
+
+        pub fn to_value(&self) -> Result<Value, ConversionError> {
+            Ok(serde_json::to_value(&self.inner)?)
+        }
+    }
+
+    impl<T> TryFrom<Value> for EmbedJson<T> {
+        type Error = ConversionError;
+
+        fn try_from(value: Value) -> Result<Self, Self::Error> {
+            Self::from_value(value)
+        }
     }
 }
 
@@ -119,9 +146,27 @@ mod tests {
     #[cfg(feature = "serde")]
     use super::*;
 
+    #[test]
+    fn embed_json_string() {
+        #[derive(Debug, PartialEq, Eq)]
+        struct V {
+            val: i32,
+        }
+
+        struct TestEmbed {
+            v: EmbedJson<V>,
+        }
+
+        let json = r#"{"val":  5}"#;
+        let embed = TestEmbed {
+            v: EmbedJson::from_string(json.to_string()),
+        };
+        assert_eq!(embed.v.to_string(), json);
+    }
+
     #[cfg(feature = "serde")]
     #[test]
-    fn embed_round_trip() {
+    fn embed_json_round_trip() {
         #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
         struct V {
             val: i32,
