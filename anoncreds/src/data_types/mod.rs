d3d917a2be0dd632682f9d4f0018e9db2dfa2fd9
Darko Kulic <darko.kulic@sicpa.com>
2022-11-16 10:24:27 +0100

    Removed merkle_tree code
    Signed-off-by: Darko Kulic <darko.kulic@sicpa.com>

--- anoncreds/src/data_types/mod.rs
+++ anoncreds/src/data_types/mod.rs
@@ -16,10 +16,6 @@ pub use ursa;
 /// Type definitions related Indy credential issuance and verification
 pub mod anoncreds;
 
-#[cfg(feature = "merkle_tree")]
-/// Patricia Merkle tree support
-pub mod merkle_tree;
-
 mod identifiers;
 
 pub use identifiers::cred_def::*;
