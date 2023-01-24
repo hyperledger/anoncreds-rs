871f7099dc86d220dbf3fdb2cf107ecc382fdda2
blu3beri <blu3beri@proton.me>
2023-01-24 15:13:37 +0100

    chore(js): set the correct version
    Work funded by the Government of Ontario.
    
    Signed-off-by: blu3beri <blu3beri@proton.me>

--- wrappers/javascript/anoncreds-nodejs/test/bindings.test.ts
+++ wrappers/javascript/anoncreds-nodejs/test/bindings.test.ts
@@ -8,7 +8,7 @@ describe('bindings', () => {
   test('version', () => {
     const version = anoncreds.version()
 
-    expect(version).toEqual('0.3.1')
+    expect(version).toEqual('0.1.0-dev.1')
   })
 
   test('current error', () => {
