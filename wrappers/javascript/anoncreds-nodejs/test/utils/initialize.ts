bd072a7a615d43aeeb46fec317b7ef0a2f6fc84a
Ariel Gentile <gentilester@gmail.com>
2023-02-09 14:08:33 -0300

    fix: don't set native default logger
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- wrappers/javascript/anoncreds-nodejs/test/utils/initialize.ts
+++ wrappers/javascript/anoncreds-nodejs/test/utils/initialize.ts
@@ -1,9 +1,7 @@
 import { registerAnoncreds } from '@hyperledger/anoncreds-shared'
 
 import { NodeJSAnoncreds } from '../../src/NodeJSAnoncreds'
-import { nativeAnoncreds } from '../../src/library'
 
 export const setup = () => {
   registerAnoncreds({ lib: new NodeJSAnoncreds() })
-  nativeAnoncreds.anoncreds_set_default_logger()
 }
