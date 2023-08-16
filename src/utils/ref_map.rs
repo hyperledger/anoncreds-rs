9f49ef0fcafe23ee3b2efdaf3b70edb78cf66508
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2023-08-16 19:34:49 +0300

    Simplified the ReferencesMap iterators
    Signed-off-by: Bogdan Mircea <mirceapetrebogdan@gmail.com>

--- src/utils/ref_map.rs
+++ src/utils/ref_map.rs
@@ -1,4 +1,8 @@
-use std::{borrow::Borrow, collections::HashMap, hash::Hash};
+use std::{
+    borrow::Borrow,
+    collections::{hash_map, HashMap},
+    hash::{self, Hash},
+};
 
 /// Trait primarily added to accommodate both native and FFI usage of maps.
 ///
@@ -10,7 +14,7 @@ use std::{borrow::Borrow, collections::HashMap, hash::Hash};
 /// references from the one we already have.
 pub trait ReferencesMap<K, V>
 where
-    K: std::hash::Hash + Eq,
+    K: hash::Hash + Eq,
 {
     type Iter<'a>: IntoIterator<Item = (&'a K, &'a V)>
     where
@@ -29,9 +33,9 @@ where
 
 impl<K, V> ReferencesMap<K, V> for HashMap<K, V>
 where
-    K: std::hash::Hash + Eq,
+    K: hash::Hash + Eq,
 {
-    type Iter<'a> = std::collections::hash_map::Iter<'a, K, V>
+    type Iter<'a> = hash_map::Iter<'a, K, V>
     where
         K: 'a,
         V: 'a,
@@ -51,14 +55,21 @@ where
     }
 }
 
-type MapFnBothRef<'a, K, V> = fn((&'a &'a K, &'a &'a V)) -> (&'a K, &'a V);
-type MapIterBothRef<'a, K, V> = std::collections::hash_map::Iter<'a, &'a K, &'a V>;
+pub struct MapIterBothRef<'a, K, V>(hash_map::Iter<'a, &'a K, &'a V>);
+
+impl<'a, K, V> Iterator for MapIterBothRef<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (*k, *v))
+    }
+}
 
 impl<K, V> ReferencesMap<K, V> for HashMap<&K, &V>
 where
-    K: std::hash::Hash + Eq,
+    K: hash::Hash + Eq,
 {
-    type Iter<'a> = std::iter::Map<MapIterBothRef<'a, K, V>, MapFnBothRef<'a, K, V>>
+    type Iter<'a> = MapIterBothRef<'a, K, V>
     where
         K: 'a,
         V: 'a,
@@ -74,18 +85,25 @@ where
     }
 
     fn iter_ref(&self) -> Self::Iter<'_> {
-        self.iter().map(|(k, v)| (*k, *v))
+        MapIterBothRef(self.iter())
     }
 }
 
-type MapFnKeyRef<'a, K, V> = fn((&'a &'a K, &'a V)) -> (&'a K, &'a V);
-type MapIterKeyRef<'a, K, V> = std::collections::hash_map::Iter<'a, &'a K, V>;
+pub struct MapIterKeyRef<'a, K, V>(hash_map::Iter<'a, &'a K, V>);
+
+impl<'a, K, V> Iterator for MapIterKeyRef<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (*k, v))
+    }
+}
 
 impl<K, V> ReferencesMap<K, V> for HashMap<&K, V>
 where
-    K: std::hash::Hash + Eq,
+    K: hash::Hash + Eq,
 {
-    type Iter<'a> = std::iter::Map<MapIterKeyRef<'a, K, V>, MapFnKeyRef<'a, K, V>>
+    type Iter<'a> = MapIterKeyRef<'a, K, V>
     where
         K: 'a,
         V: 'a,
@@ -101,18 +119,25 @@ where
     }
 
     fn iter_ref(&self) -> Self::Iter<'_> {
-        self.iter().map(|(k, v)| (*k, v))
+        MapIterKeyRef(self.iter())
     }
 }
 
-type MapFnValueRef<'a, K, V> = fn((&'a K, &'a &'a V)) -> (&'a K, &'a V);
-type MapIterValueRef<'a, K, V> = std::collections::hash_map::Iter<'a, K, &'a V>;
+pub struct MapIterValueRef<'a, K, V>(hash_map::Iter<'a, K, &'a V>);
+
+impl<'a, K, V> Iterator for MapIterValueRef<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (k, *v))
+    }
+}
 
 impl<K, V> ReferencesMap<K, V> for HashMap<K, &V>
 where
     K: std::hash::Hash + Eq,
 {
-    type Iter<'a> = std::iter::Map<MapIterValueRef<'a, K, V>, MapFnValueRef<'a, K, V>>
+    type Iter<'a> = MapIterValueRef<'a, K ,V>
     where
         K: 'a,
         V: 'a,
@@ -128,6 +153,6 @@ where
     }
 
     fn iter_ref(&self) -> Self::Iter<'_> {
-        self.iter().map(|(k, v)| (k, *v))
+        MapIterValueRef(self.iter())
     }
 }
