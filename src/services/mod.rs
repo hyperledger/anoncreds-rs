ee0dc133b5bea745fbb9a4b3fa3fcdf4de8264b2
Berend Sliedrecht <blu3beri@proton.me>
2023-09-09 11:27:09 +0200

    fix cargo issues and test work without default-features
    Signed-off-by: Berend Sliedrecht <blu3beri@proton.me>

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,11 +1,6 @@
-mod helpers;
-
+pub(crate) mod helpers;
 pub mod issuer;
 pub mod prover;
 pub mod tails;
 pub mod types;
 pub mod verifier;
-
-pub mod utils {
-    pub use super::helpers::encode_credential_attribute;
-}
