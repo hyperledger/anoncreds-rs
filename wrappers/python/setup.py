68df5accfc5a69e3756dc199c9d44fa9fc781dae
Stephen Curran <swcurran@gmail.com>
2026-01-06 15:39:38 -0800

    Update after move to new GitHub org
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         author_email="anoncreds@lists.hyperledger.org",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/hyperledger/anoncreds-rs",
+        url="https://github.com/anoncreds/anoncreds-rs",
         packages=find_packages(),
         include_package_data=True,
         package_data={
